b261b352657916e16cc36d10419d160869b87b8d
JorgeAlfredoMastrandrea <81812907+JorgeAlfredoMastrandrea@users.noreply.github.com>
2024-06-12 17:19:14 -0300

    -2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='data_analysis_framework',
+    name='python_data_analysis',
     version='0.1',
     packages=find_packages(),
     install_requires=[

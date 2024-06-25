bab8c55ee16a49560c668d069fb66345ece417a7
JorgeAlfredoMastrandrea <81812907+JorgeAlfredoMastrandrea@users.noreply.github.com>
2024-06-25 20:19:03 -0300

    -

--- src/models/abstract_dataframe.py
+++ src/models/abstract_dataframe.py
@@ -13,3 +13,16 @@ class AbstractDataFrame(ABC):
     @abstractmethod
     def filter_data(self):
         pass
+
+    def agrupar_por_criterio(dataframe , listaDeColumnas , agg_dict , reset_index):
+        # esta función lo que va a hacer es agrupar un dataframe usando diferentes criterios,
+        # el dFrame no está agrupado, en la lista de columnas están los criterios para agrupar
+        # el agg_dict es la función matemática que vamos a usar en este caso siempre es count..
+        # y el reset_index es un boolean que me va a indicar si necesito resetear el índice o no
+        # esto sirve para cuando tenga que calcular los desempeños con dataframe que ya se han agrupado    
+        dataframe.reset_index(drop=True, inplace=True)
+        if reset_index :
+            dataframe = dataframe.groupby(listaDeColumnas).agg(agg_dict).reset_index()
+        else:
+            dataframe = dataframe.groupby(listaDeColumnas).agg(agg_dict)
+        return dataframe
